33646df045e1741f9bc145ed6ac0dc37c8aa45c0
DShafir <66061459+DShafir@users.noreply.github.com>
2020-05-28 18:17:52 +0700

    Add files via upload

---
+++ Form_Ekspedisi/Form3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Ekspedisi
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "user" && textBox2.Text == "user")
+            {
+                Form1 a = new Form1();
+                a.Show();
+                this.Hide();
+            }
+            else
+            {
+                string error = "Username atau Password Salah";
+                MessageBox.Show(error);
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            string error = "Username: user / password: user";
+            MessageBox.Show(error);
+        }
+    }
+}
